5b8f59c5a15188b970da08e4c66975ea4c113447
Vasco Nunes <vbnunes@criticalsoftware.com>
2018-11-07 17:39:52 +0000

    Introducing Kompsat-5 capability

--- src/main/app-resources/pre_processing/run.sh
+++ src/main/app-resources/pre_processing/run.sh
@@ -117,6 +117,12 @@ function check_product_type() {
       [[ "$prodTypeName" != "L1G" ]] && return $ERR_WRONGPRODTYPE
   fi
 
+  if [ ${mission} = "Kompsat-5"  ]; then
+      prodTypeName=$(ls ${retrievedProduct}/*L??.tif | head -1 | sed -n -e 's|^.*_\(.*\).tif$|\1|p')
+      [[ -z "$prodTypeName" ]] && return ${ERR_GETPRODTYPE}
+      [[ "$prodTypeName" != "L1D" ]] && return $ERR_WRONGPRODTYPE
+  fi
+
   if [ ${mission} = "RapidEye"  ]; then
       prodTypeName=$(ls ${retrievedProduct}/*.tif | head -1 | sed -n -e 's/.*\([0-9][A-Z]*\)-.*/\1/p')
       [[ -z "$prodTypeName" ]] && return ${ERR_GETPRODTYPE}
@@ -278,6 +284,12 @@ case "$mission" in
             echo 2.8
             ;;
 
+        "Kompsat-5")
+            product_xml=$(find ${retrievedProduct}/ -name 'K5_*_Aux.xml')
+            pixSpac=$( cat ${product_xml} | grep GroundRangeGeometricResolution | sed -n -e 's|^.*<GroundRangeGeometricResolution>\(.*\)</GroundRangeGeometricResolution>|\1|p' )
+            echo  $pixSpac | awk '{ print sprintf("%.9f", $1); }'
+            ;;
+
         "Landsat-8")
             echo 30
             ;;
@@ -393,6 +405,11 @@ case "$mission" in
                 return $?
                 ;;
 
+        "Kompsat-5")
+	        pre_processing_k5 "${prodname}" "${pixelSpacing}" "${pixelSpacingMaster}" "${performCropping}" "${subsettingBoxWKT}"
+            return $?
+	    ;;
+
         *)
 	    return "${ERR_CALLPREPROCESS}"
 	    ;;
@@ -419,6 +436,12 @@ case "$mission" in
         "RapidEye")
             bandListCsv="Blue,Green,Red,RedEdge,NIR"
 	    ;;
+
+	    "Kompsat-5")
+            # naming convention is K5>_<YYYYMMDDhhmmss>_<tttttt>_<nnnnn>_<o>_<MM><SS>_<PP>_<LLL> where PP is the polarization
+            # always single pol
+            bandListCsv=${prodname:38:2}
+        ;;
 
         *)
             return "${ERR_BAND_LIST}"
@@ -555,7 +578,7 @@ ciop-log "INFO" "Generated request file: ${SNAP_REQUEST}"
 ciop-log "INFO" "Invoking SNAP-gpt on the generated request file for Sentinel 1 data pre processing"
 
 # invoke the ESA SNAP toolbox
-/opt/snap/bin/gpt $SNAP_REQUEST -c "2048M" &> /dev/null
+gpt $SNAP_REQUEST -c "2048M" &> /dev/null
 # check the exit code
 [ $? -eq 0 ] || return $ERR_SNAP
 
@@ -840,6 +863,141 @@ gpt $SNAP_REQUEST -c "${CACHE_SIZE}" &> /dev/null
 # check the exit code
 [ $? -eq 0 ] || return $ERR_SNAP
 
+# create a tar archive where DIM output product is stored and put it in OUTPUT dir
+cd ${TMPDIR}
+tar -cf ${outProdBasename}.tar ${outProdBasename}.d*
+#tar -cjf ${outProdBasename}.tar -C ${TMPDIR} .
+mv ${outProdBasename}.tar ${OUTPUTDIR}
+rm -rf ${outProdBasename}.d*
+cd -
+
+}
+
+# Pre processing function for Kompsat-5
+function pre_processing_k5() {
+# function call pre_processing_k5 "${prodname}" "${pixelSpacing}" "${pixelSpacingMaster}" "${performCropping}" "${subsettingBoxWKT}"
+
+inputNum=$#
+[ "$inputNum" -ne 5 ] && return ${ERR_PREPROCESS}
+
+local prodname=$1
+local pixelSpacing=$2
+local pixelSpacingMaster=$3
+local performCropping=$4
+local subsettingBoxWKT=$5
+local index=0
+prodBasename=$(basename ${prodname})
+local imgFile=""
+imgFile=$(find ${prodname}/ -name 'K5_*_L1D.tif')
+# remove metadata from tif to avoid that SNAP-gpt uses the K5 reader (that doesn't work) while converting into DIM
+gdal_edit.py -unsetmd ${imgFile}
+[ $? -eq 0 ] || return ${ERR_GDAL}
+# rename data to avoid that SNAP-gpt uses the K5 reader (that doesn't work) while converting into DIM
+file2convert=${TMPDIR}/product2convert.tif
+mv ${imgFile} ${file2convert}
+# convert tif to beam dimap format
+ciop-log "INFO" "Invoking SNAP-pconvert on the generated request file for tif to dim conversion"
+pconvert -f dim -o ${TMPDIR} ${file2convert}
+# check the exit code
+[ $? -eq 0 ] || return $ERR_SNAP
+# get translated product name
+imgFileDIM=$(find ${TMPDIR} -name '*.dim')
+# define output name of DIM product with K5 file converted into dB
+imgFileDIM_dB=${TMPDIR}/${prodBasename}_dB.dim
+ciop-log "INFO" "Preparing SNAP request file for dB scaling"
+SNAP_REQUEST=$( create_snap_request_linear_to_dB "${imgFileDIM}" "${imgFileDIM_dB}" )
+[ $? -eq 0 ] || return ${SNAP_REQUEST_ERROR}
+[ $DEBUG -eq 1 ] && cat ${SNAP_REQUEST}
+# report activity in the log
+ciop-log "INFO" "Generated request file: ${SNAP_REQUEST}"
+# report activity in the log
+ciop-log "INFO" "Invoking SNAP-gpt on the generated request file for dB scaling"
+# invoke the ESA SNAP toolbox
+gpt $SNAP_REQUEST -c "${CACHE_SIZE}" &> /dev/null
+# check the exit code
+[ $? -eq 0 ] || return $ERR_SNAP
+# remove temp file product in beam dimap
+rm -rf "${imgFileDIM%.dim}.d*"
+# get bands name
+currentBandsList=$( xmlstarlet sel -t -v "/Dimap_Document/Image_Interpretation/Spectral_Band_Info/BAND_NAME" ${imgFileDIM_dB} )
+currentBandsList=(${currentBandsList})
+currentBandsList_num=${#currentBandsList[@]}
+currentBandsListTXT=${TMPDIR}/currentBandsList.txt
+# loop on band names to fill band list
+let "currentBandsList_num-=1"
+for index in `seq 0 $currentBandsList_num`;
+do
+    if [ $index -eq 0  ] ; then
+        echo ${currentBandsList[${index}]} > ${currentBandsListTXT}
+    else
+        echo  ${currentBandsList[${index}]} >> ${currentBandsListTXT}
+    fi
+done
+# loop over known product bands to fill target bands list
+targetBandsNamesListTXT=${TMPDIR}/targetBandsNamesList.txt
+# source bands list for Pleiades
+sourceBandsList=$(get_band_list "${prodBasename}" "Kompsat-5" )
+# convert band from comma separted values to space separated values
+bandListSsv=$( echo "${sourceBandsList}" | sed 's|,| |g' )
+# convert ssv to array
+declare -a bandListArray=(${bandListSsv})
+# get number of bands
+numBands=${#bandListArray[@]}
+local bid=0
+let "numBands-=1"
+for bid in `seq 0 $numBands`; do
+    if [ $bid -eq 0  ] ; then
+        echo ${bandListArray[$bid]} > ${targetBandsNamesListTXT}
+    else
+        echo ${bandListArray[$bid]} >> ${targetBandsNamesListTXT}
+    fi
+done
+# build request file for rename all the bands contained into the product
+# report activity in the log
+outProdRename=${TMPDIR}/output_renamed_bands
+ciop-log "INFO" "Preparing SNAP request file for bands renaming"
+# prepare the SNAP request
+SNAP_REQUEST=$( create_snap_request_rename_all_bands "${imgFileDIM_dB}" "${currentBandsListTXT}" "${targetBandsNamesListTXT}" "${outProdRename}")
+[ $? -eq 0 ] || return ${SNAP_REQUEST_ERROR}
+[ $DEBUG -eq 1 ] && cat ${SNAP_REQUEST}
+# report activity in the log
+ciop-log "INFO" "Generated request file: ${SNAP_REQUEST}"
+# report activity in the log
+ciop-log "INFO" "Invoking SNAP-gpt on the generated request file bands renaming"
+# invoke the ESA SNAP toolbox
+gpt $SNAP_REQUEST -c "${CACHE_SIZE}" &> /dev/null
+# check the exit code
+[ $? -eq 0 ] || return $ERR_SNAP
+# remove temp calibrated file product in beam dimap
+rm -rf "${imgFileDIM_dB%.dim}.d*"
+
+# use the greter pixel spacing as target spacing (in order to downsample if needed, upsampling always avoided)
+local target_spacing=$( get_greater_pixel_spacing ${pixelSpacing} ${pixelSpacingMaster} )
+# check for resampling operator: to be used only if the resolution is differenet from the current product one
+local performResample=""
+if (( $(bc <<< "$target_spacing != $pixelSpacing") )) ; then
+    performResample="true"
+else
+    performResample="false"
+fi
+outProdBasename=${prodBasename}_pre_proc
+outProd=${TMPDIR}/${outProdBasename}
+
+# report activity in the log
+ciop-log "INFO" "Preparing SNAP request file for data pre processing"
+# prepare the SNAP request
+SNAP_REQUEST=$( create_snap_request_rsmpl_rprj_sbs "${outProdRename}.dim" "${performResample}" "${target_spacing}" "${performCropping}" "${subsettingBoxWKT}" "${sourceBandsList}" "${outProd}")
+[ $? -eq 0 ] || return ${SNAP_REQUEST_ERROR}
+[ $DEBUG -eq 1 ] && cat ${SNAP_REQUEST}
+# report activity in the log
+ciop-log "INFO" "Generated request file: ${SNAP_REQUEST}"
+# report activity in the log
+ciop-log "INFO" "Invoking SNAP-gpt on the generated request file for data pre processing"
+# invoke the ESA SNAP toolbox
+gpt $SNAP_REQUEST -c "${CACHE_SIZE}" 2> log.txt
+returncode=$?
+test_txt=$(cat log.txt | grep "No intersection")
+#rm -rf ${outProdRename}.d* ${targetBandsNamesListTXT}
 # create a tar archive where DIM output product is stored and put it in OUTPUT dir
 cd ${TMPDIR}
 tar -cf ${outProdBasename}.tar ${outProdBasename}.d*
@@ -1484,6 +1642,96 @@ cd
 
 }
 
+function create_snap_request_linear_to_dB(){
+# function call: create_snap_request_linear_to_dB "${inputfile}" "${outputfile}"
+
+# function which creates the actual request from
+# a template and returns the path to the request
+
+# get number of inputs
+inputNum=$#
+# check on number of inputs
+if [ "$inputNum" -ne "2" ] ; then
+    return ${SNAP_REQUEST_ERROR}
+fi
+
+local inputfile=$1
+local outputfile=$2
+local filename="${outputfile##*/}"
+local ext="${filename##*.}"
+local format=""
+if [[ "$ext" == "dim" ]]; then
+    format="BEAM-DIMAP"
+elif [[ "$ext" == "tif" ]]; then
+    format="GeoTIFF-BigTIFF"
+else
+    return ${SNAP_REQUEST_ERROR}
+fi
+
+
+#sets the output filename
+snap_request_filename="${TMPDIR}/$( uuidgen ).xml"
+
+cat << EOF > ${snap_request_filename}
+<graph id="Graph">
+  <version>1.0</version>
+  <node id="Read">
+    <operator>Read</operator>
+    <sources/>
+    <parameters class="com.bc.ceres.binding.dom.XppDomElement">
+      <file>${inputfile}</file>
+    </parameters>
+  </node>
+  <node id="BandMaths">
+    <operator>BandMaths</operator>
+    <sources>
+      <sourceProduct refid="Read"/>
+    </sources>
+    <parameters class="com.bc.ceres.binding.dom.XppDomElement">
+      <targetBands>
+        <targetBand>
+          <name>band_1</name>
+          <type>uint8</type>
+          <expression>if fneq(band_1,0) then max(20*log10(band_1),1) else 0</expression>
+          <description/>
+          <unit/>
+          <noDataValue>0.0</noDataValue>
+        </targetBand>
+      </targetBands>
+      <variables/>
+    </parameters>
+  </node>
+  <node id="Write">
+    <operator>Write</operator>
+    <sources>
+      <sourceProduct refid="BandMaths"/>
+    </sources>
+    <parameters class="com.bc.ceres.binding.dom.XppDomElement">
+      <file>${outputfile}</file>
+      <formatName>${format}</formatName>
+    </parameters>
+  </node>
+  <applicationData id="Presentation">
+    <Description/>
+    <node id="Read">
+            <displayPosition x="37.0" y="134.0"/>
+    </node>
+    <node id="BandMaths">
+      <displayPosition x="247.0" y="132.0"/>
+    </node>
+    <node id="Write">
+            <displayPosition x="455.0" y="135.0"/>
+    </node>
+  </applicationData>
+</graph>
+EOF
+
+    [ $? -eq 0 ] && {
+        echo "${snap_request_filename}"
+        return 0
+    } || return ${SNAP_REQUEST_ERROR}
+
+}
 
 function create_snap_request_stack(){
 # function call: create_snap_request_stack "${inputfiles_list}" "${outProdDIM}" "${numProd}"
