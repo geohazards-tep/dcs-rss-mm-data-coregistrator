efae0d9acddbe9b1c9aea4a1530f2883eaddbe43
Vasco Nunes <vbnunes@criticalsoftware.com>
2019-02-11 13:39:33 +0000

    fixing the installation of L8_reflectance

--- src/main/scripts/post_install.sh
+++ src/main/scripts/post_install.sh
@@ -2,7 +2,7 @@
 conda update conda -y
 conda install --file /application/dependencies/packages.list
 export PATH=/opt/anaconda/bin:$PATH
-pip install rio-toa
+#pip install rio-toa
 git clone -b develop https://github.com/ESRIN-RSS/L8_reflectance.git
-cd L8_reflectance
+#cd L8_reflectance
 pip install -e L8_reflectance
